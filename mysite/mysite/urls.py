749e0686e5c1257c8c0d6b13a408f10406b97b12
ppan <820566997@qq.com>
2019-02-27 18:42:34 +0800

    获取用户信息接口

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
